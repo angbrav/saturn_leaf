1618b45743b40aa3c1b176d417e60e7abbd08618
angbrav <manuelbravogestoso@gmail.com>
2016-02-02 17:51:42 +0100

    Add Apache 2.0 license

--- src/saturn_simple_backend_vnode.erl
+++ src/saturn_simple_backend_vnode.erl
@@ -1,3 +1,24 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015-2016 INESC-ID, Instituto Superior Tecnico,
+%%                         Universidade de Lisboa, Portugal
+%% Copyright (c) 2015-2016 Universite Catholique de Louvain, Belgium
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%  
+%% -------------------------------------------------------------------
 -module(saturn_simple_backend_vnode).
 -behaviour(riak_core_vnode).
 -include("saturn_leaf.hrl").
