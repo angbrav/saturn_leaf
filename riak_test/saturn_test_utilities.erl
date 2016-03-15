5eb43a43b96d7f64ade8156e24a645caad721209
angbrav <manuelbravogestoso@gmail.com>
2016-03-15 20:36:13 +0100

    Adapt test utilities for Practi

--- riak_test/saturn_test_utilities.erl
+++ riak_test/saturn_test_utilities.erl
@@ -35,7 +35,7 @@ eventual_read(Key, Node, ExpectedResult) ->
 eventual_read(Key, Node, ExpectedResult, Clock) ->
     Result=rpc:call(Node, saturn_leaf, read, [Key, Clock]),
     case Result of
-        {ok, {ExpectedResult, _Clock}} -> Result;
+        {ok, ExpectedResult} -> Result;
         _ ->
             lager:info("I read: ~p, expecting: ~p",[Result, ExpectedResult]),
             timer:sleep(500),
