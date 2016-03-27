a93ddf557622d80709b24150a785fc99ae55690f
angbrav <manuelbravogestoso@gmail.com>
2016-03-27 14:41:45 +0200

    Changed default groups file

--- include/saturn_leaf.hrl
+++ include/saturn_leaf.hrl
@@ -5,7 +5,7 @@
 -define(SIMPLE_MASTER, saturn_simple_backend_vnode_master).
 -define(SIMPLE_SERVICE, saturn_simple_backend).
 
--define(GROUPSFILE, "data/manager/groups_file_simple.saturn").
+-define(GROUPSFILE, "data/manager/groups_facebook.saturn").
 -define(TREEFILE, "data/manager/tree_file_simple.saturn").
 -define(TREEFILE_TEST, "../include/tree_file_test.saturn").
 -define(GROUPSFILE_TEST, "../include/groups_file_test.saturn").
