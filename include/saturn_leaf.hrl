5f41c74c952a14166603ff03b019db3906a2d247
angbrav <manuelbravogestoso@gmail.com>
2016-05-04 17:05:27 +0200

    add files for three nodes experiment

--- include/saturn_leaf.hrl
+++ include/saturn_leaf.hrl
@@ -5,8 +5,8 @@
 -define(SIMPLE_MASTER, saturn_simple_backend_vnode_master).
 -define(SIMPLE_SERVICE, saturn_simple_backend).
 
--define(GROUPSFILE, "data/manager/groups_file_simple.saturn").
--define(TREEFILE, "data/manager/tree_file_simple.saturn").
+-define(GROUPSFILE, "data/manager/groups.saturn").
+-define(TREEFILE, "data/manager/tree.saturn").
 -define(TREEFILE_TEST, "../include/tree_file_test.saturn").
 -define(GROUPSFILE_TEST, "../include/groups_file_test.saturn").
 
