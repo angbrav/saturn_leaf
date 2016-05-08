4d5e818676214ec65c795cae839ceaef35b3d369
angbrav <manuelbravogestoso@gmail.com>
2016-05-08 14:26:55 +0200

    Adjust hb and clock computation freq

--- include/saturn_leaf.hrl
+++ include/saturn_leaf.hrl
@@ -25,8 +25,8 @@
 -define(MYIDPREFIX, {prefix, myid_prefix}).
 -define(MYIDKEY, myid_key).
 
--define(HEARTBEAT_FREQ, 1000).
--define(TIMES_FREQ, 1000).
+-define(HEARTBEAT_FREQ, 5).
+-define(TIMES_FREQ, 5).
 
 -define(PROPAGATION_MODE, naive_erlang).
 %-define(PROPAGATION_MODE, short_tcp).
