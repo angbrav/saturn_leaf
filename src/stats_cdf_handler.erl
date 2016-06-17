521f4d88b2d0d788693f78f97a16ad6ec5060234
angbrav <manuelbravogestoso@gmail.com>
2016-06-17 17:13:46 +0200

    Fix bug

--- src/stats_cdf_handler.erl
+++ src/stats_cdf_handler.erl
@@ -52,19 +52,26 @@ compute_cdf(Data, From, Type) ->
     generate_percentiles(List, 1, ListSteps, []).
 
 get_liststeps(List) ->
-    Total = orddict:size(List),
+    Total = compute_size(List, 0),
     Step = Total / ?PERCENTILES,
     ListSteps = lists:foldl(fun(Number, Acc) ->
                                 Acc ++ [trunc(Step*Number)]
-                              end, [], lists:seq(1, ?PERCENTILES-1)),
+                            end, [], lists:seq(1, ?PERCENTILES-1)),
     ListSteps ++ [Total].
+
+compute_size([], Counter) ->
+    Counter;
+
+compute_size([Next|Rest], Counter)->
+    {_Time, List} = Next,
+    compute_size(Rest, Counter + length(List)).
 
 get_ordered(From, Table, Id) ->
     lists:foldl(fun(Key, List0) ->
                     [{Key, {Sender, Time}}] = ets:lookup(Table, Key),
                     case Sender of
                         From ->
-                            orddict:store(Time, v, List0);
+                            orddict:append(Time, v, List0);
                         _ ->
                             List0
                     end
@@ -73,12 +80,14 @@ get_ordered(From, Table, Id) ->
 generate_percentiles([], _Counter, [], Times) ->
     Times;
 
-generate_percentiles([Next|Rest], Counter, [Counter|RestSteps], Times) ->
-    {Time, v} = Next,
-    generate_percentiles(Rest, Counter+1, RestSteps, Times ++ [Time]);
-    
-generate_percentiles([_Next|Rest], Counter, Steps, Times) ->
-    generate_percentiles(Rest, Counter+1, Steps, Times).
+generate_percentiles([Next|Rest], Counter, [NextStep|RestSteps]=Steps, Times) ->
+    {Time, List} = Next,
+    case (length(List) + Counter) >= NextStep of
+        true ->
+            generate_percentiles(Rest, Counter+length(List), RestSteps, Times ++ [Time]);
+        false ->
+            generate_percentiles(Rest, Counter+length(List), Steps, Times)
+    end.
 
 -ifdef(TEST).
 
@@ -89,27 +98,27 @@ get_ordered_test() ->
     true = ets:insert(Table, {2, {sender2, 70}}),
     true = ets:insert(Table, {3, {sender1, 90}}),
     true = ets:insert(Table, {4, {sender1, 10}}),
-    ?assertEqual([{10, v}, {30, v}, {60, v}, {90, v}], get_ordered(sender1, Table, 5)).
+    ?assertEqual([{10, [v]}, {30, [v]}, {60, [v]}, {90, [v]}], get_ordered(sender1, Table, 5)).
     
 generate_liststeps_test() ->
-    List = [{1, v}, {2, v}, {3, v},
-            {4, v}, {5, v}, {6, v}, 
-            {7, v}, {8, v}, {9, v},
-            {10, v}, {11, v}, {12, v}, 
-            {13, v}, {14, v}, {15, v}, 
-            {16, v}, {17, v}, {18, v}, 
-            {19, v}, {20, v}, {21, v}],
+    List = [{1, [v]}, {2, [v]}, {3, [v]},
+            {4, [v]}, {5, [v]}, {6, [v]}, 
+            {7, [v]}, {8, [v]}, {9, [v]},
+            {10, [v]}, {11, [v]}, {12, [v]}, 
+            {13, [v]}, {14, [v]}, {15, [v]}, 
+            {16, [v]}, {17, [v]}, {18, [v]}, 
+            {19, [v]}, {20, [v]}, {21, [v]}],
     ?assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21], get_liststeps(List)).
 
 generate_percentiles_test() ->
-    List = [{10, v}, {20, v}, {30, v},
-            {40, v}, {50, v}, {60, v}, 
-            {70, v}, {80, v}, {90, v},
-            {100, v}, {110, v}, {120, v}, 
-            {130, v}, {140, v}, {150, v}, 
-            {160, v}, {170, v}, {180, v}, 
-            {190, v}, {200, v}, {210, v}],
+    List = [{10, [v]}, {20, [v]}, {30, [v]},
+            {40, [v]}, {50, [v]}, {60, [v]}, 
+            {70, [v]}, {80, [v]}, {90, [v]},
+            {100, [v]}, {110, [v]}, {120, [v]}, 
+            {130, [v]}, {140, [v]}, {150, [v]}, 
+            {160, [v]}, {170, [v]}, {180, [v]}, 
+            {190, [v]}, {200, [v]}, {210, [v]}],
     ListSteps = get_liststeps(List),
-    ?assertEqual([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 210], generate_percentiles(List, 1, ListSteps, [])).
+    ?assertEqual([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 210], generate_percentiles(List, 0, ListSteps, [])).
 
 -endif.
