181e3c84df9cf74a409a48794a5a9d22c31d2311
angbrav <manuelbravogestoso@gmail.com>
2016-07-12 13:50:26 +0200

    Modify to work in new propagtion, no in charge of data now

--- src/saturn_leaf_converger.erl
+++ src/saturn_leaf_converger.erl
@@ -32,12 +32,10 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          code_change/3, terminate/2]).
 -export([handle/2,
-         clean_state/1,
-         dump_stats/3]).
+         clean_state/1]).
 
 -record(state, {labels_queue :: queue(),
-                ops,
-                staleness,
+                min_pending,
                 myid}).
                
 reg_name(MyId) ->  list_to_atom(integer_to_list(MyId) ++ atom_to_list(?MODULE)). 
@@ -52,56 +50,39 @@ handle(MyId, Message) ->
 clean_state(MyId) ->
     gen_server:call({global, reg_name(MyId)}, clean_state, infinity).
 
-dump_stats(MyId, Sender, Type) ->
-    gen_server:call({global, reg_name(MyId)}, {dump_stats, Sender, Type}, infinity).
-
 init([MyId]) ->
-    Ops = ets:new(operations_converger, [set, named_table, private]),
-    Staleness = ?STALENESS:init(),
     Name = list_to_atom(integer_to_list(MyId) ++ "converger_queue"),
     {ok, #state{labels_queue=ets_queue:new(Name),
-                ops=Ops,
-                staleness=Staleness,
+                min_pending=[infinity],
                 myid=MyId}}.
 
-handle_call(clean_state, _From, S0=#state{ops=Ops, staleness=Staleness, labels_queue=Labels}) ->
-    true = ets:delete(Ops),
-    Ops1 = ets:new(operations_converger, [set, named_table, private]),
-    Staleness1 = ?STALENESS:clean(Staleness),
-    {reply, ok, S0#state{ops=Ops1, labels_queue=ets_queue:clean(Labels), staleness=Staleness1}};
+handle_call(clean_state, _From, S0=#state{labels_queue=Labels}) ->
+    {reply, ok, S0#state{labels_queue=ets_queue:clean(Labels), min_pending=[infinity]}}.
 
-handle_call({dump_stats, Sender, Type}, _From, S0=#state{staleness=Staleness}) ->
-    Stats = ?STALENESS:compute_cdf(Staleness, Sender, Type),
-    {reply, {ok, Stats}, S0}.
-
-handle_cast(completed, S0=#state{labels_queue=Labels0, ops=Ops, staleness=Staleness}) ->
-    {_, Labels1} = ets_queue:out(Labels0),
-    {Labels2, Staleness1} =  handle_label(ets_queue:peek(Labels1), Labels1, Ops, Staleness),
-    {noreply, S0#state{labels_queue=Labels2, staleness=Staleness1}};
-    
-handle_cast({new_stream, Stream, _SenderId}, S0=#state{labels_queue=Labels0, ops=Ops, staleness=Staleness}) ->
-    %lager:info("New stream received. Label: ~p", Stream),
-    Empty = ets_queue:is_empty(Labels0),
-    Labels1 = lists:foldl(fun(Label, Queue) ->
-                            ets_queue:in(Label, Queue)
-                          end, Labels0, Stream),
-    case Empty of
+handle_cast({completed, TimeStamp}, S0=#state{labels_queue=Labels0, min_pending=[OldH|_]=Min0}) ->
+    [NewH|_] = Min1 = lists:delete(TimeStamp, Min0),
+    case (NewH>OldH) of
         true ->
-            {Labels2, Staleness1} =  handle_label(ets_queue:peek(Labels1), Labels1, Ops, Staleness),
-            {noreply, S0#state{labels_queue=Labels2, staleness=Staleness1}};
+            {Min2, Labels1} = dealwith_pending(ets_queue:peek(Labels0), Labels0, Min1),
+            {noreply, S0#state{min_pending=Min2, labels_queue=Labels1}};
         false ->
-            {noreply, S0#state{labels_queue=Labels1}}
+            {noreply, S0#state{min_pending=Min1}}
     end;
 
-handle_cast({new_operation, Label, Value}, S0=#state{labels_queue=Labels0, ops=Ops, staleness=Staleness}) ->
-    %lager:info("New operation received. Label: ~p", [Label]),
-    case ets_queue:peek(Labels0) of
-        {value, Label} ->
-            Staleness1 = execute_operation(Label, Value, Staleness),
-            {noreply, S0#state{staleness=Staleness1}};
-        _ ->
-            true = ets:insert(Ops, {Label, Value}),
-            {noreply, S0}
+handle_cast({new_stream, Stream, _SenderId}, S0=#state{labels_queue=Labels0, min_pending=Min0}) ->
+    %lager:info("New stream received. Label: ~p", Stream),
+    case ets_queue:is_empty(Labels0) of
+        true ->
+            {Min1, Stream1} = dealwith_stream(Stream, Min0),
+            Labels1 = lists:foldl(fun(Label, Queue) ->
+                                    ets_queue:in(Label, Queue)
+                                  end, Labels0, Stream1),
+            {noreply, S0#state{labels_queue=Labels1, min_pending=Min1}};
+        false ->
+            Labels1 = lists:foldl(fun(Label, Queue) ->
+                                    ets_queue:in(Label, Queue)
+                                  end, Labels0, Stream),
+            {noreply, S0#state{labels_queue=Labels1}}
     end;
 
 handle_cast(_Info, State) ->
@@ -116,32 +97,51 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-execute_operation(Label, Value, Staleness) ->
-    %lager:info("New operation to be executed. Label: ~p", [Label]),
-    Staleness1 = ?STALENESS:add_update(Staleness, Label),
-    BKey = Label#label.bkey,
-    Clock = Label#label.timestamp,
-    DocIdx = riak_core_util:chash_key(BKey),
-    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, ?PROXY_SERVICE),
-    [{IndexNode, _Type}] = PrefList,
-    saturn_proxy_vnode:propagate(IndexNode, BKey, Value, Clock),
-    Staleness1.
-    
+dealwith_pending(empty, Labels, Min) ->
+    {Min, Labels};
 
-handle_label(empty, Queue, _Ops, Staleness) ->
-    {Queue, Staleness};
+dealwith_pending({value, Label}, Labels0, [Min|_]=ListMin) ->
+    TimeStamp = Label#label.timestamp,
+    case (TimeStamp =< Min) of
+        true ->
+            {{value, Label}, Labels1} = ets_queue:out(Labels0),
+            case handle_label(Label) of
+                true ->
+                    dealwith_pending(ets_queue:peek(Labels1), Labels1, [TimeStamp|ListMin]);
+                false ->
+                    dealwith_pending(ets_queue:peek(Labels1), Labels1, ListMin)
+            end;
+        false ->
+            {Min, Labels0}
+    end.
 
-handle_label({value, Label}, Queue, Ops, Staleness) ->
+dealwith_stream([], Min) ->
+    {Min, []};
+
+dealwith_stream([Label|Rest], [Min|_]=ListMin) ->
+    TimeStamp = Label#label.timestamp,
+    case (TimeStamp =< Min) of
+        true ->
+            case handle_label(Label) of
+                true ->
+                    dealwith_stream(Rest, [TimeStamp|ListMin]);
+                false ->
+                    dealwith_stream(Rest, ListMin)
+            end;
+        false ->
+            {Min, Rest}
+    end.
+
+handle_label(Label) ->
     case Label#label.operation of
         remote_read ->
             BKey = Label#label.bkey,
             DocIdx = riak_core_util:chash_key(BKey),
             PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, ?PROXY_SERVICE),
-            Staleness1 = ?STALENESS:add_remote(Staleness, Label),
+            %Staleness1 = ?STALENESS:add_remote(Staleness, Label),
             [{IndexNode, _Type}] = PrefList,
             saturn_proxy_vnode:remote_read(IndexNode, Label),
-            {_, Queue1} = ets_queue:out(Queue),
-            handle_label(ets_queue:peek(Queue1), Queue1, Ops, Staleness1);
+            false;
         remote_reply ->
             Payload = Label#label.payload,
             Client = Payload#payload_reply.client,
@@ -155,18 +155,24 @@ handle_label({value, Label}, Queue, Ops, Staleness) ->
                     BKey = Payload#payload_reply.bkey,
                     gen_fsm:send_event(Client, {new_value, BKey, Value})
             end,
-            {_, Queue1} = ets_queue:out(Queue),
-            handle_label(ets_queue:peek(Queue1), Queue1, Ops, Staleness);
+            false;
         update ->
-            case ets:lookup(Ops, Label) of
-                [{Label, Value}] ->
-                    Staleness1 = execute_operation(Label, Value, Staleness),
-                    true = ets:delete(Ops, Label),
-                    {Queue, Staleness1};
-                [] ->
-                    %lager:info("Operation not received for label: ~p", [Label]),
-                    {Queue, Staleness}
-            end
+            BKey = Label#label.bkey,
+            Clock = Label#label.timestamp,
+            Node = Label#label.node,
+            DocIdx = riak_core_util:chash_key(BKey),
+            PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, ?PROXY_SERVICE),
+            [{IndexNode, _Type}] = PrefList,
+            saturn_proxy_vnode:propagate(IndexNode, BKey, Clock, Node),
+            true;
+        write_tx ->
+            [BKey|_Tail] = BKeys = Label#label.bkey,
+            Clock = Label#label.timestamp,
+            DocIdx = riak_core_util:chash_key(BKey),
+            PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, ?PROXY_SERVICE),
+            [{IndexNode, _Type}] = PrefList,
+            saturn_proxy_vnode:remote_write_tx(IndexNode, BKeys, Clock),
+            true
     end.
 
 -ifdef(TEST).
