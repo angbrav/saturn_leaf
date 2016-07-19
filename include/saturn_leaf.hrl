e98ec5b33926366377b9457e45f6ce8e8350ee6c
angbrav <manuelbravogestoso@gmail.com>
2016-07-19 19:42:31 +0200

    Add stabilization time

--- include/saturn_leaf.hrl
+++ include/saturn_leaf.hrl
@@ -25,7 +25,8 @@
 -define(MYIDPREFIX, {prefix, myid_prefix}).
 -define(MYIDKEY, myid_key).
 
--define(HEARTBEAT_FREQ, 1000).
+-define(HEARTBEAT_FREQ, 5). %millisec
+-define(STABILIZATION_FREQ, 0). %microsec
 
 %-define(PROPAGATION_MODE, naive_erlang).
 -define(PROPAGATION_MODE, bypass_tree).
