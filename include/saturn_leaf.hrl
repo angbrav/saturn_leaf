9ea0429bd67aeafb6f889ddcc44c1700b16e9427
angbrav <manuelbravogestoso@gmail.com>
2016-09-21 18:39:14 +0200

    hb 10

--- include/saturn_leaf.hrl
+++ include/saturn_leaf.hrl
@@ -25,7 +25,7 @@
 -define(MYIDPREFIX, {prefix, myid_prefix}).
 -define(MYIDKEY, myid_key).
 
--define(HEARTBEAT_FREQ, 100).
+-define(HEARTBEAT_FREQ, 10).
 -define(TIMES_FREQ, 5).
 
 -define(PROPAGATION_MODE, naive_erlang).
