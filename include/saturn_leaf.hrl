64169f8c03f0b0267ed67f7025811a17549f9d7a
angbrav <manuelbravogestoso@gmail.com>
2017-08-16 20:01:48 +0200

    change hb interval

--- include/saturn_leaf.hrl
+++ include/saturn_leaf.hrl
@@ -26,7 +26,7 @@
 -define(MYIDKEY, myid_key).
 
 -define(HEARTBEAT_FREQ, 2).
--define(INTER_HEARTBEAT_FREQ, 500).
+-define(INTER_HEARTBEAT_FREQ, 5).
 -define(STABILIZATION_FREQ, 1).
 -define(STABILIZATION_FREQ_CONVERGER, 1).
 
